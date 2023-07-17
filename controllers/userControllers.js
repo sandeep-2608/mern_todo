93faca846543e8d4bd41799b982c4a9b6b6adf44
Sandeep Yadav <sandeep.sy999@gmail.com>
2023-07-17 12:20:41 +0530

    fixed engine

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,7 @@
 import { User } from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/features.js";
+import ErrorHandler from "../middlewares/error.js";
 
 export const getAllUsers = async (req, res) => {};
 
